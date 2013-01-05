c3f4c032d5fe15302bd4a25db2c619d63c398bd2
Zack Mulgrew <maxamor@gmail.com>
2013-01-05 00:05:17 -0800

    Made SpriteAnimator safer
    Missing a check for null; if animation pointer was null then things blow
    up.

--- engine/src/hikari/core/game/SpriteAnimator.cpp
+++ engine/src/hikari/core/game/SpriteAnimator.cpp
@@ -28,34 +28,36 @@ namespace hikari {
     void SpriteAnimator::update(float delta) {
         Animator::update(delta);
 
-        const AnimationFrame &currentFrame = getAnimation()->getFrameAt(getCurrentFrameIndex());
-        const Rectangle2D<int> &currentFrameRectangle = currentFrame.getSourceRectangle();
+        if(const auto& animation = getAnimation()) {
+            const auto& currentFrame = animation->getFrameAt(getCurrentFrameIndex());
+            const auto& currentFrameRectangle = currentFrame.getSourceRectangle();
 
-        sourceRectangle.top = currentFrameRectangle.getTop();
-        sourceRectangle.width = currentFrameRectangle.getWidth();
-        sourceRectangle.height = currentFrameRectangle.getHeight();
-        sourceRectangle.left = currentFrameRectangle.getLeft();
+            sourceRectangle.top = currentFrameRectangle.getTop();
+            sourceRectangle.width = currentFrameRectangle.getWidth();
+            sourceRectangle.height = currentFrameRectangle.getHeight();
+            sourceRectangle.left = currentFrameRectangle.getLeft();
 
-        sprite.setTextureRect(sourceRectangle);
+            sprite.setTextureRect(sourceRectangle);
         
-        sprite.setOrigin(
-            static_cast<float>(currentFrame.getHotspot().getX()),
-            static_cast<float>(currentFrame.getHotspot().getY())
-        ); 
-
-        if(invertXOffset) {
             sprite.setOrigin(
-                static_cast<float>(currentFrame.getSourceRectangle().getWidth() - currentFrame.getHotspot().getX()),
-                static_cast<float>(sprite.getOrigin().y)
+                static_cast<float>(currentFrame.getHotspot().getX()),
+                static_cast<float>(currentFrame.getHotspot().getY())
             ); 
-        } 
 
-        if(invertYOffset) {
-            sprite.setOrigin(
-                static_cast<float>(sprite.getOrigin().x),
-                static_cast<float>(currentFrame.getSourceRectangle().getHeight() - currentFrame.getHotspot().getY())
-            ); 
-        } 
+            if(invertXOffset) {
+                sprite.setOrigin(
+                    static_cast<float>(currentFrame.getSourceRectangle().getWidth() - currentFrame.getHotspot().getX()),
+                    static_cast<float>(sprite.getOrigin().y)
+                ); 
+            } 
+
+            if(invertYOffset) {
+                sprite.setOrigin(
+                    static_cast<float>(sprite.getOrigin().x),
+                    static_cast<float>(currentFrame.getSourceRectangle().getHeight() - currentFrame.getHotspot().getY())
+                ); 
+            }
+        }
     }
 
 } // hikari
