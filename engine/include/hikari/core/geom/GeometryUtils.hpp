8e2fe8e56300fa49830f22259eb1c1b9cf3651b1
Zack Mulgrew <maxamor@gmail.com>
2013-02-18 17:18:09 -0800

    Added GeometryUtils, general purpose util
    For geometry-related operations. Right now allows testing intersection
    between BoundingBox and Rectangle2D as well as containment.

---
+++ engine/include/hikari/core/geom/GeometryUtils.hpp
@@ -0,0 +1,65 @@
+#ifndef HIKARI_CORE_GEOM_GEOMETRYUTILS
+#define HIKARI_CORE_GEOM_GEOMETRYUTILS
+
+#include "hikari/core/geom/BoundingBox.hpp"
+#include "hikari/core/geom/Rectangle2D.hpp"
+
+namespace hikari {
+namespace geom {
+
+    /**
+     * Checks if a bounding box fully contains a given rectangle.
+     * @param  box  a BoundingBox (the "container")
+     * @param  rect a Rectangle2D (the "containee")
+     * @return      true if rect is inside box
+     */
+    template <typename T>
+    bool contains(const BoundingBox<T>& box, const Rectangle2D<T>& rect) {
+        return (box.contains(rect.getLeft(), rect.getTop()) &&
+                box.contains(rect.getRight(), rect.getTop()) &&
+                box.contains(rect.getLeft(), rect.getBottom()) &&
+                box.contains(rect.getRight(), rect.getBottom()));
+    }
+
+    /**
+     * Checks if a bounding box intersects with a rectangle.
+     * @param  box  a BoundingBox
+     * @param  rect a Rectangle2D
+     * @return      true if box and rect overlap
+     */
+    template <typename T>
+    bool intersects(const BoundingBox<T>& box, const Rectangle2D<T>& rect) {
+        return !(box.getLeft() > rect.getRight() || box.getRight() < rect.getLeft() ||
+                box.getTop() > rect.getBottom() || box.getBottom() < rect.getTop());
+    }
+
+    /**
+     * Checks if a rectangle fully contains a given bounding box.
+     * @param  rect a Rectangle2D (the "containee")
+     * @param  box  a BoundingBox (the "container")
+     * @return      true if box is inside rect
+     */
+    template <typename T>
+    bool contains(const Rectangle2D<T>& rect, const BoundingBox<T>& box) {
+        return (rect.contains(box.getLeft(), box.getTop()) &&
+                rect.contains(box.getRight(), box.getTop()) &&
+                rect.contains(box.getLeft(), box.getBottom()) &&
+                rect.contains(box.getRight(), box.getBottom()));
+    }
+
+    /**
+     * Checks if a rectangle intersects with a bounding box.
+     * @param  rect a Rectangle2D
+     * @param  box  a BoundingBox
+     * @return      true if rect and box overlap
+     */
+    template <typename T>
+    bool intersects(const Rectangle2D<T>& rect, const BoundingBox<T>& box) {
+        return !(rect.getLeft() > box.getRight() || rect.getRight() < box.getLeft() ||
+                rect.getTop() > box.getBottom() || rect.getBottom() < box.getTop());
+    }
+
+} // hikari::geom
+} // hikari
+
+#endif HIKARI_CORE_GEOM_GEOMETRYUTILS
