1e3c3591203e5abaf966362dcf6360095798b7c4
Zack Mulgrew <maxamor@gmail.com>
2013-02-19 19:24:54 -0800

    Movable enhancements for gravity and limits
    Added configurable limit for vertical speed of all Movable objects.
    Added math::clamp to clamp a value between a high and low value.

--- engine/include/hikari/core/math/MathUtils.hpp
+++ engine/include/hikari/core/math/MathUtils.hpp
@@ -12,14 +12,12 @@ namespace math {
         @return 1 if positive, -1 if negative, 0 if 0
     */
     template <class T>
-    inline T sign(const T& number)
-    {
+    inline T sign(const T& number) {
         return (number == 0) ? 0 : (number < 0) ? -1 : 1;
     }
 
     template<>
-    inline float sign(const float& number)
-    {
+    inline float sign(const float& number) {
         return (number == 0.0f) ? 0.0f : (number < 0.0f) ? -1.0f : 1.0f;
     }
 
@@ -29,8 +27,7 @@ namespace math {
         @return 0 if number is positive, 1 if number is negative.
     */
     template <class T>
-    inline int signbit(const T& number)
-    {
+    inline int signbit(const T& number) {
         return (number < 0) ? 1 : 0;
     }
 
@@ -38,9 +35,25 @@ namespace math {
         TODO: Document this function.
     */
     template <class T>
-    inline T copysign(const T& number, const T& signSource)
-    {
+    inline T copysign(const T& number, const T& signSource) {
         return std::abs(number) * hikari::math::sign(signSource);
+    }
+
+    /**
+     * @brief Returns the value "value" brought into the range [ lo, hi ].
+     * 
+     * If the value is greater than "hi", return hi. If the value is
+     * less than "lo", return lo. Otherwise, return the original value.
+     *
+     * @param value   The value to be clamped
+     * @param low     The low point of the range to be clamped to
+     * @param high    The high point of the range to be clamped to
+     */
+    template <typename T>
+    T clamp(const T& value, const T& low, const T& high) {
+        // Alternatively:
+        // return std::max(std::min(value, high), low);
+        return value < low ? low : (value > high ? high : value);
     }
 
 }
