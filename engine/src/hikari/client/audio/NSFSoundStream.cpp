3e3eb4674a4208e43a5921a4e3b25d3ffc5b2e93
zackhehuman <zack@zackthehuman.com>
2013-06-15 01:34:22 -0700

    Made multi-sample mixing possible.

--- engine/src/hikari/client/audio/NSFSoundStream.cpp
+++ engine/src/hikari/client/audio/NSFSoundStream.cpp
@@ -8,14 +8,22 @@
 
 namespace hikari {
 
-    NSFSoundStream::NSFSoundStream(std::size_t bufferSize) :
+    NSFSoundStream::NSFSoundStream(std::size_t bufferSize, unsigned int samplerCount) :
     sf::SoundStream(),
     myBufferSize(bufferSize),
     myBuffer    (new short[myBufferSize]),
     emu         (nullptr),
     trackInfo   (nullptr),
+    samplerCount(samplerCount),
+    activeSampler(0),
     mutex       ()
     {
+        activeSampler = 0;
+
+        for(int i = 0; i < samplerCount; ++i) {
+            sampleBuffers.push_back(std::unique_ptr<short[]>(new short[myBufferSize]));
+        }
+
         this->stop();
     }
 
@@ -52,6 +60,17 @@ namespace hikari {
             }
 
             emu.reset(file_type->new_emu());
+
+            for(int i = 0; i < samplerCount; ++i) {
+                auto sampleEmu = std::unique_ptr<Music_Emu>(file_type->new_emu());
+
+                sampleEmu->set_sample_rate(SAMPLE_RATE);
+                gme_load_data(sampleEmu.get(), buffer.get(), length);
+                sampleEmu->start_track(-1);
+                sampleEmu->ignore_silence(false);
+                sampleEmus.push_back(std::move(sampleEmu));
+            }
+
             trackInfo.reset(new track_info_t());
 
             if(!emu.get()) {
@@ -67,20 +86,7 @@ namespace hikari {
         }
 
         initialize(2, SAMPLE_RATE);
-        setCurrentTrack(0);
-
-        /*int count = emu->track_count();
-        for(int i = 0; i < count; i++) {
-            track_info_t info;
-
-            emu->track_info(&info, i);
-
-            std::string song(info.song);
-            std::string comment(info.comment);
-
-            std::cout << i << '\t' <<
-                song << '\t' << comment << std::endl;
-        }*/
+        //setCurrentTrack(0);
 
         return true;
     }
@@ -92,20 +98,44 @@ namespace hikari {
 
     bool NSFSoundStream::onGetData(sf::SoundStream::Chunk& Data) {
         sf::Lock lock(mutex);
-        handleError(emu->play(myBufferSize, myBuffer.get()));
+
+        auto * mixedBuffer = myBuffer.get();
+
+         for(int bufferIndex = 0; bufferIndex < samplerCount; ++bufferIndex) {
+            if(!sampleEmus[bufferIndex]->track_ended()) {
+                sampleEmus[bufferIndex]->play(myBufferSize, sampleBuffers[bufferIndex].get());
+            }
+        }
+
+        // Mix buffers
+        for(int i = 0; i < myBufferSize; ++i) {
+            short mixedValue = 0;
+
+            for(int bufferIndex = 0; bufferIndex < samplerCount; ++bufferIndex) {
+                // Don't mix buffers that aren't playing.
+                if(!sampleEmus[bufferIndex]->track_ended()) {
+                    mixedValue += sampleBuffers[bufferIndex].get()[i];
+                } else {
+                    // Zero-out any buffer that isn't playing.
+                    //sampleBuffers[bufferIndex].get()[i] = 0;
+                }
+            }
+
+            mixedBuffer[i] = mixedValue;
+        }
 
         // HIKARI_LOG(debug) << "onGetData";
 
         Data.samples   = &myBuffer[0];
         Data.sampleCount = myBufferSize;
 
-        if(!emu->track_ended()) {
+        //if(!emu->track_ended()) {
             // HIKARI_LOG(debug) << "onGetData ... track not over";
             return true;
-        } else {
+        //} else {
             // HIKARI_LOG(debug) << "onGetData ... track over";
-            return false;
-        }
+        //    return false;
+        //}
     }
 
     void NSFSoundStream::handleError(const char* str) const {
@@ -126,6 +156,11 @@ namespace hikari {
         sf::Lock lock(mutex);
         if(track >= 0 && track < getTrackCount()) {
             emu->start_track(track);
+
+            sampleEmus[activeSampler]->start_track(track);
+
+            activeSampler++;
+            activeSampler %= samplerCount;
         }
     }
 
