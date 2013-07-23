be4a2c5993da38e6b3b67b1691370cae24ee4a6a
zackhehuman <zack@zackthehuman.com>
2013-07-22 21:50:36 -0700

    Added new props to ClientConfig and added serialize

--- engine/include/hikari/client/ClientConfig.hpp
+++ engine/include/hikari/client/ClientConfig.hpp
@@ -1,6 +1,9 @@
 #ifndef HIKARI_CLIENT_CLIENTCONFIG
 #define HIKARI_CLIENT_CLIENTCONFIG
 
+#include <map>
+#include <string>
+
 namespace Json {
     class Value;
 }
@@ -13,19 +16,41 @@ namespace hikari {
         static const char* PROPERTY_FPS;
         static const char* PROPERTY_SCRIPTING;
         static const char* PROPERTY_SCRIPTING_STACKSIZE;
+        static const char* PROPERTY_VIDEOMODE;
+        static const char* PROPERTY_BINDINGS;
+        static const char* PROPERTY_KEYBOARD_BINDINGS;
 
         bool enableVsync;
         bool enableFpsDisplay;
         unsigned int stackSize;
+        std::string videoMode;
+        std::map<std::string, std::string> keybindings;
 
         void extractValuesFromJson(const Json::Value& configJson);
     public:
         ClientConfig();
         ClientConfig(const Json::Value& configJson);
 
-        const bool isVsyncEnabled() const;
-        const bool isFpsDisplayEnabled() const;
-        const unsigned int getScriptingStackSize() const;
+        bool isVsyncEnabled() const;
+        void setVsyncEnabled(bool enabled);
+
+        bool isFpsDisplayEnabled() const;
+        void setFpsDisplayEnabled(bool enabled);
+
+        unsigned int getScriptingStackSize() const;
+
+        std::string getVideoMode() const;
+        void setVideoMode(const std::string & mode);
+
+        std::map<std::string, std::string> getKeyBindings() const;
+        void setKeyBindings(const std::map<std::string, std::string> & bindings);
+
+        /**
+         * Serializes the configuration options into a string.
+         *
+         * @return string representation of the config
+         */
+        std::string serialize();
     };
 
 } // hikari
--- engine/src/hikari/client/ClientConfig.cpp
+++ engine/src/hikari/client/ClientConfig.cpp
@@ -1,11 +1,18 @@
 #include "hikari/client/ClientConfig.hpp"
+
+#include <algorithm>
+
 #include <json/value.h>
+#include <json/writer.h>
 
 namespace hikari {
     const char* ClientConfig::PROPERTY_VSYNC = "vsync";
     const char* ClientConfig::PROPERTY_FPS = "showfps";
     const char* ClientConfig::PROPERTY_SCRIPTING = "scripting";
     const char* ClientConfig::PROPERTY_SCRIPTING_STACKSIZE = "stackSize";
+    const char* ClientConfig::PROPERTY_VIDEOMODE = "videoMode";
+    const char* ClientConfig::PROPERTY_BINDINGS = "bindings";
+    const char* ClientConfig::PROPERTY_KEYBOARD_BINDINGS = "keyboard";
 
     void ClientConfig::extractValuesFromJson(const Json::Value& configJson) {
         if(!configJson.isNull()) {
@@ -20,10 +27,35 @@ namespace hikari {
             // Extract scripting config values
             //
             if(configJson.isMember(PROPERTY_SCRIPTING)) {
-                auto scriptingConfigJson = configJson.get(PROPERTY_SCRIPTING, Json::Value());
+                const Json::Value & scriptingConfigJson = configJson.get(PROPERTY_SCRIPTING, Json::Value());
 
                 if(scriptingConfigJson.isMember(PROPERTY_SCRIPTING_STACKSIZE)) {
                     stackSize = static_cast<unsigned int>(scriptingConfigJson.get(PROPERTY_SCRIPTING_STACKSIZE, 1024).asUInt());
+                }
+            }
+
+            if(configJson.isMember(PROPERTY_VIDEOMODE)) {
+                videoMode = configJson.get(PROPERTY_VIDEOMODE, "1x").asString();
+            }
+
+            if(configJson.isMember(PROPERTY_BINDINGS)) {
+                const Json::Value & bindingsObject = configJson[PROPERTY_BINDINGS];
+
+                if(bindingsObject.isMember(PROPERTY_KEYBOARD_BINDINGS)) {
+                    const Json::Value & keyboardBindingObject = bindingsObject[PROPERTY_KEYBOARD_BINDINGS];
+                    const Json::Value::Members & keyboardBindingKeys = keyboardBindingObject.getMemberNames();
+                 
+                    for(unsigned int i = 0, length = keyboardBindingKeys.size(); i < length; i++) {
+                        const std::string & key = keyboardBindingKeys[i];
+                        
+                        //
+                        // TODO: Additional checking to make sure these keys are valid buttons
+                        //
+
+                        keybindings.insert(
+                            std::make_pair(key, keyboardBindingObject[key].asString())
+                        );
+                    }
                 }
             }
         }
@@ -33,6 +65,8 @@ namespace hikari {
         : enableVsync(false)
         , enableFpsDisplay(false)
         , stackSize(1024)
+        , videoMode("1x")
+        , keybindings()
     {
 
     }
@@ -41,19 +75,74 @@ namespace hikari {
         : enableVsync(false)
         , enableFpsDisplay(false)
         , stackSize(1024)
+        , videoMode("1x")
+        , keybindings()
     {
         extractValuesFromJson(configJson);
     }
 
-    const bool ClientConfig::isVsyncEnabled() const {
+    bool ClientConfig::isVsyncEnabled() const {
         return enableVsync;
     }
 
-    const bool ClientConfig::isFpsDisplayEnabled() const {
+    void ClientConfig::setVsyncEnabled(bool enabled) {
+        enableVsync = enabled;
+    }
+
+    bool ClientConfig::isFpsDisplayEnabled() const {
         return enableFpsDisplay;
     }
 
-    const unsigned int ClientConfig::getScriptingStackSize() const {
+    void ClientConfig::setFpsDisplayEnabled(bool enabled) {
+        enableFpsDisplay = enabled;
+    }
+
+    unsigned int ClientConfig::getScriptingStackSize() const {
         return stackSize;
     }
+
+    std::string ClientConfig::getVideoMode() const {
+        return videoMode;
+    }
+
+    void ClientConfig::setVideoMode(const std::string & mode) {
+        videoMode = mode;
+    }
+
+    std::map<std::string, std::string> ClientConfig::getKeyBindings() const {
+        return keybindings;
+    }
+
+    void ClientConfig::setKeyBindings(const std::map<std::string, std::string> & bindings) {
+        keybindings = bindings;
+    }
+
+    std::string ClientConfig::serialize() {
+        Json::StyledWriter writer;
+        Json::Value container(Json::objectValue);
+
+        container[PROPERTY_VSYNC] = isVsyncEnabled();
+        container[PROPERTY_VIDEOMODE] = getVideoMode();
+        container[PROPERTY_FPS] = isFpsDisplayEnabled();
+        container[PROPERTY_SCRIPTING] = Json::Value(Json::objectValue);
+        container[PROPERTY_SCRIPTING][PROPERTY_SCRIPTING_STACKSIZE] = getScriptingStackSize();
+        
+        // Write all of the keybindings to an object and then attach it
+        Json::Value keybindingObject(Json::objectValue);
+
+        std::for_each(
+            std::begin(keybindings),
+            std::end(keybindings),
+            [&](const decltype(keybindings)::value_type & entry) {
+                const std::string & key = entry.first;
+                const std::string & value = entry.second;
+                keybindingObject[key] = value;
+            }
+        );
+
+        container[PROPERTY_BINDINGS] = keybindingObject;
+
+        return writer.write(container);
+    }
+
 } // hikari
