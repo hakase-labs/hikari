9f74390d31fc6aeb5834520925f4b87a3327dfd5
zackhehuman <zack@zackthehuman.com>
2013-07-25 20:56:05 -0700

    Fixed unnecessary copying of BoundingBox
    MIssing a &!

--- engine/src/hikari/client/game/objects/Hero.cpp
+++ engine/src/hikari/client/game/objects/Hero.cpp
@@ -140,7 +140,7 @@ namespace hikari {
 
             // Check to see if we're touching a ladder
             {
-                auto bbox = this->getBoundingBox();
+                const auto & bbox = this->getBoundingBox();
                 auto topRight = bbox.getTopRight();
                 auto topLeft = bbox.getTopLeft();
                 auto bottomLeft = bbox.getBottomLeft();
