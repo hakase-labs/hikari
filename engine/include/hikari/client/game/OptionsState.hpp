db41c8ec0f3099b27d349b58b97501797b84681b
zackhehuman <zack@zackthehuman.com>
2014-04-16 17:28:45 -0700

    Reworking options state, menu items for audio settings

--- engine/include/hikari/client/game/OptionsState.hpp
+++ engine/include/hikari/client/game/OptionsState.hpp
@@ -31,12 +31,21 @@ namespace hikari {
 
     class OptionsState : public GameState {
     private:
+        static const std::string AUDIO_LABEL_TEXT;
+        static const std::string MUSIC_LABEL_TEXT;
+        static const std::string SAMPLE_LABEL_TEXT;
+
         std::string name;
         GameController & controller;
         std::weak_ptr<GuiService> guiService;
         std::shared_ptr<Input> input;
         std::unique_ptr<gcn::Container> guiContainer;
         std::unique_ptr<gcn::Label> guiLabel;
+        std::unique_ptr<gcn::Label> audioLabel;
+        std::unique_ptr<gcn::Label> musicLabel;
+        std::unique_ptr<gcn::Label> musicVolumeLabel;
+        std::unique_ptr<gcn::Label> sampleLabel;
+        std::unique_ptr<gcn::Label> sampleVolumeLabel;
         std::unique_ptr<gui::Menu> guiMenu;
         std::unique_ptr<gcn::ActionListener> guiActionListener;
         std::unique_ptr<gcn::SelectionListener> guiSelectionListener;
