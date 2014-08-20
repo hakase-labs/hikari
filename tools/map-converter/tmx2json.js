9d5aede75a72bdfdcbdd478050ac086e5fa4c91f
zackhehuman <zack@zackthehuman.com>
2014-08-19 23:25:07 -0700

    Starting room parsing

--- tools/map-converter/tmx2json.js
+++ tools/map-converter/tmx2json.js
@@ -11,6 +11,8 @@ var parser = new xml2js.Parser({
 
 parser.addListener('end', function(result) {
   console.log(JSON.stringify(extractMapMetaData(result), undefined, 1));
+
+  console.log(JSON.stringify(extractRoom(result), undefined, 1));
 });
 
 fs.readFile(__dirname + '/dev-map.tmx', function(err, data) {
@@ -27,6 +29,7 @@ function extractMapMetaData(tmxJson) {
     tmxMap,
     tmxMeta,
     tmxProperties,
+    tmxTileset,
     maybeMusicId,
     maybeBossEntity;
 
@@ -50,6 +53,13 @@ function extractMapMetaData(tmxJson) {
       maybeBossEntity = unwrapProperty(tmxProperties, 'bossEntity');
       result.bossEntity = maybeBossEntity;
     }
+
+    if(_.has(tmxMap, 'tileset')) {
+      tmxTileset = _.flatten(tmxMap.tileset);
+      tmxTileset = tmxTileset[0].$;
+
+      result.tileset = tmxTileset.name;
+    }
   }
 
   return result;
@@ -69,4 +79,54 @@ function unwrapProperty(obj, propName) {
   }
 
   return undefined;
+}
+
+/**
+ * Maps weird TMX properties into a normal JS Object.
+ * @param  {Object} tmxJson - weird TMX JSON
+ * @return {Object} a POJO of key/values from the properties input
+ */
+function mapProperties(tmxJson) {
+  return _.assign.apply(this, _.map(tmxJson, function(prop) {
+    var result = {};
+
+    result[prop.$.name] = prop.$.value;
+
+    return result;
+  }));
+}
+
+function extractRoom(tmxJson) {
+  var result,
+    layers = tmxJson.map.layer,
+    properties,
+    tileLayer,
+    attrLayer,
+    TILE_LAYER = 0,
+    ATTR_LAYER = 1;
+
+  result = {};
+
+  if(layers) {
+    tileLayer = layers[TILE_LAYER];
+    attrLayer = layers[ATTR_LAYER];
+    properties = mapProperties(tileLayer.properties[0].property);
+
+    result.width = parseInt(tileLayer.$.width, 10);
+    result.height = parseInt(tileLayer.$.height, 10);
+    result.x = parseInt(properties.x, 10);
+    result.y = parseInt(properties.y, 10);
+
+    if(_.has(properties, 'heroSpawnX')) {
+      result.heroSpawnX = parseInt(properties.heroSpawnX, 10);
+    }
+
+    if(_.has(properties, 'heroSpawnY')) {
+      result.heroSpawnY = parseInt(properties.heroSpawnY, 10);
+    }
+  }
+
+  console.log(JSON.stringify(layers, undefined, 1));
+
+  return result;
 }
