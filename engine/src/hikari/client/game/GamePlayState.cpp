53715c6eeb90aef5b2f756a659ba946ad18f785f
Zack Mulgrew <zack@zackthehuman.com>
2014-11-02 23:52:09 -0800

    Add Snake Man level

--- engine/src/hikari/client/game/GamePlayState.cpp
+++ engine/src/hikari/client/game/GamePlayState.cpp
@@ -597,7 +597,7 @@ namespace hikari {
         Movable::setGravity(0.25f);
 
         std::vector<std::string> mapList;
-        mapList.push_back("map-pearlman.json");
+        mapList.push_back("map-snake.json");
         mapList.push_back("map-test6.json");
         mapList.push_back("map-pearlman.json");
         mapList.push_back("map-pearl.json");
